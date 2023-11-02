e011a3c8e98b8448b19cb3f2fcf184e44ded4bac
nahid246hasan <nahid246hasan@gmail.com>
2023-11-02 12:11:35 +0600

    Learn Get.bottomSheet() and change theme according to button press

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var color=Colors.white;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,19 +27,66 @@ class _HomeScreenState extends State<HomeScreen> {
                   title: 'Delete Chat',
                   titlePadding: EdgeInsets.all(20),
                   contentPadding: EdgeInsets.all(20),
-                  middleText: 'Are you sure you want to delete this chat',
+
+                  //middleText: 'Are you sure you want to delete this chat',
+                  //middleText e max 3 line er beshi use kora jay na
+                  // ei problem er solution hishebe amra content use korbo
+                  content:
+                      const Text('Are you sure you want to delete this chat'),
+                  //content basically widget support kore......
+
                   confirm: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Get.back(); //eta diye amra getX er madhome screen pop korte pari
+                    },
                     child: const Text("Yes"),
                   ),
-                  cancel:  TextButton(
+                  cancel: TextButton(
                     onPressed: () {},
                     child: const Text("No"),
                   ),
                 );
               },
             ),
-          )
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("GetX Bottom Sheet"),
+              subtitle: const Text("GetX Bottom sheet with getx"),
+              onTap: () {
+                Get.bottomSheet(
+                  Container(
+                    color: color,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: const Icon(Icons.light_mode),
+                          title: const Text("Light Theme"),
+                          onTap: () {
+                            setState(() {
+                              color=Colors.white;
+                            });
+                            //changeTheme change our theme
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          leading: const Icon(Icons.dark_mode),
+                          title: const Text("Dark Theme"),
+                          onTap: () {
+                            setState(() {
+                              color=Colors.black;
+                            });
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
